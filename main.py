400012c106f9fccb9fc13e6393adfb03118b4bce
n205 <54013649+n205@users.noreply.github.com>
2025-08-07 23:01:49 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI, Request, Form
-from fastapi.responses import HTMLResponse
+from fastapi.responses import HTMLResponse, JSONResponse 
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 import httpx
